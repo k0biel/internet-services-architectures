818a5d477e1acd1ebf9e9ee2c982ab2eb397c938
Jakub <162193998+k0biel@users.noreply.github.com>
2024-12-28 17:09:42 +0100

    Add files via upload

---
+++ Gateway-Application/src/main/java/org/example/GatewayConfiguration.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class GatewayConfiguration {
+
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("Warehouse-Management-Application", r -> r
+                        .path("/warehouses/**")
+                        .uri("http://localhost:8081"))
+
+                .route("Material-Management-Application", r -> r
+                        .path("/materials/**")
+                        .uri("http://localhost:8082"))
+                .build();
+    }
+
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:4200");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
